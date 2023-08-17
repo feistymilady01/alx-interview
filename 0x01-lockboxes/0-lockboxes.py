d118e2519cba52832294a87ea68b58c7bcdbee6b
Susan <pricelessblinks@gmail.com>
2023-08-17 20:56:39 +0000

    correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" You have n number of locked boxes in front of you. Each box is numbered
-sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
-"""
+"""Script will unlock list of lists"""
 
 
 def canUnlockAll(boxes):
-    """ Write a method that determines if all the boxes can be opened.
-    @boxes is a list of lists
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
+
     keys = [0]
-    for n in keys:
-        for key in boxes[n]:
-            if key not in keys and key < len(boxes):
-                keys.append(key)
-    return len(keys) == len(boxes)
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
